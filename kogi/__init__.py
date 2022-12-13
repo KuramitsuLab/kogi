6c47e3d954051f0cf7ad992ed2d6f72ab0dde4f8
kkuramitsu <kkuramitsu@gmail.com>
2022-12-14 06:23:03 +0900

    add atcoder

--- kogi/__init__.py
+++ kogi/__init__.py
@@ -1,7 +1,8 @@
 
-from .hook import enable_kogi_hook, disable_kogi_hook, kogi_register_hook
-from .service import kogi_set, kogi_print
+from .hook import enable_kogi_hook, disable_kogi_hook
+from .service import kogi_set, kogi_print, debug_print
 from .ui import rmt
+import kogi.problem
 
 set = kogi_set
 print = kogi_print
@@ -10,8 +11,3 @@ enable = enable_kogi_hook
 disable = disable_kogi_hook
 
 enable_kogi_hook()
-
-# from .problem import atcoder_detector, atcoder_judge
-# kogi_register_hook('atcoder', atcoder_judge, atcoder_detector)
-
-# from .dialog import kogi_ask as ask
--- kogi/problem/__init__.py
+++ kogi/problem/__init__.py
@@ -1,7 +1,7 @@
-import traceback
 from kogi.service import kogi_print
 from .drill import kogi_judge, judge_cpc
 from .atcoder import download_atcoder_problem
+from kogi.hook import kogi_register_hook
 
 
 def atcoder_detector(directive, raw_cell):
@@ -20,3 +20,6 @@ def atcoder_judge(ipy, raw_cell, directive, catch_and_start_kogi):
     else:
         kogi_print('問題が見つかりません。')
     return None
+
+
+kogi_register_hook('atcoder', atcoder_judge, atcoder_detector)
