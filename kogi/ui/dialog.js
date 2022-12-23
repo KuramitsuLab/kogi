517fa4f0a6567243199ca0a642e9c2a23b9c348b
kkuramitsu <kkuramitsu@gmail.com>
2022-12-24 03:33:25 +0900

    fix

--- kogi/ui/dialog.js
+++ kogi/ui/dialog.js
@@ -1,3 +1,14 @@
+var like = (id, score) => {
+    google.colab.kernel.invokeFunction('notebook.like', [id, score], {});
+};
+var copy = (id) => {
+    const text = document.getElementById(`t${id}`).value;
+    navigator.clipboard.writeText(text);
+};
+var say = (prompt, bid) => {
+    const text = document.getElementById(`b${bid}`).textContent;
+    google.colab.kernel.invokeFunction('notebook.say', [prompt, text], {});
+};
 var inputPane = document.getElementById("inputXYZ");
 inputPane.addEventListener('keydown', (e) => {
     if (e.keyCode == 13) {
@@ -9,15 +20,3 @@ inputPane.addEventListener('keydown', (e) => {
 inputPane.addEventListener('focusin', (e) => {
     inputPane.style.height = 200;
 });
-var like = (id, score) => {
-    google.colab.kernel.invokeFunction('notebook.like', [id, score], {});
-};
-var copy = (id) => {
-    const text = document.getElementById(`t${id}`).value;
-    navigator.clipboard.writeText(text);
-    //google.colab.kernel.invokeFunction('notebook.like', [id], {});
-};
-var say = (prompt, bid) => {
-    const text = document.getElementById(`b${bid}`).textContent;
-    google.colab.kernel.invokeFunction('notebook.say', [prompt, text], {});
-};
