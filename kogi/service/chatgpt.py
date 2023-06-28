fd4d17990bdb05527337f6cf229d08534cf1c9cb
kkuramitsu <kkuramitsu@gmail.com>
2023-06-29 03:08:27 +0900

    fix: runtime error in openai

--- kogi/service/chatgpt.py
+++ kogi/service/chatgpt.py
@@ -21,9 +21,9 @@ def model_chat(messages: list):
 
 def model_prompt(prompt, context='', **kwargs):
     global model_cache
-    input_text = f'{context}{prompt}'
-    if input_text in model_cache:
-        return model_cache[input_text], 0
+    key = f'{context}{prompt}'
+    if key in model_cache:
+        return model_cache[key], 0
     try:
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
@@ -34,10 +34,16 @@ def model_prompt(prompt, context='', **kwargs):
         )
         used_tokens = response["usage"]["total_tokens"]
         res = response.choices[0]["message"]["content"].strip()
-        model_cache[input_text] = res
+        model_cache[key] = res
         return res, used_tokens
     except openai.error.AuthenticationError as e:
         print("""
 import kogi
 kogi.set(openai_key="") # 自分のOpenAI キーを設定してください。""")
+        return '', 0
+    except openai.error.ServiceUnavailableError as e:
+        print(e)
+        return '', 0
+    except Exception as e:
+        print(e)
         return '', 0
