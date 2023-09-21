234c8ba9691447cef08e58d1b7735f124946c548
Kimio Kuramitsu <kkuramitsu@gmail.com>
2023-09-22 05:41:21 +0900

    fix: wrong import in __init__.py

--- kogi/service/__init__.py
+++ kogi/service/__init__.py
@@ -2,20 +2,10 @@ from .globals import kogi_defined, kogi_get, globals_update
 from .s3logging import kogi_print, print_nop, debug_print, record_log
 from .textra import translate, is_ja, EJ, is_japanese_text, is_english_text
 # from .slack import load_slack, slack_send
-from .chatgpt import set_openai, model_prompt
+# from .OLDchatgpt import set_openai, model_prompt
 # from .flaskapi import load_model, model_generate, check_awake, start_server
 from .codellm import llm_prompt, llm_login
 from .simplemsg import simplify
 
 def kogi_set(**kwargs):
     globals_update(kwargs)
-    # if 'model_id' in kwargs:
-    #     load_model(kwargs['model_id'])
-    # if 'mt_key' in kwargs:
-    #     load_mt(kwargs['mt_key'])
-    # if 'slack_key' in kwargs:
-    #     load_slack(kwargs['slack_key'])
-    # if 'textra_key' in kwargs:
-    #     load_mt(kwargs['textra_key'])
-    # if 'openai_key' in kwargs:
-    #     set_openai(kwargs['openai_key'])
